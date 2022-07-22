80ec90e93c3a9d69d7ed71a8c2743fc109d0d8a7
Your Name <you@example.com>
2022-07-22 15:01:44 +0200

    first commit from front

---
+++ delivery/src/Components/AddButton/AddButton.jsx
@@ -0,0 +1,33 @@
+import React, {useEffect, useState, Fragment} from 'react';
+import Card from '../Card/Card';
+import "./AddButton.css"
+
+
+
+const AddButton = () => {
+  const [dishesList, setDishesList]=useState([]);
+
+  useEffect(()=>{
+
+    const url = "http://localhost:5000/api/dishes/"
+    const fetchAllDishes = async () => {
+      const response = await fetch(url)
+  
+        const json = await response.json()
+        setDishesList(json)
+        console.log(json)               
+      };
+      fetchAllDishes()
+      console.log(dishesList)
+  }, [])
+
+  return <div className='addButton'>
+    {  
+      dishesList.map (item=> 
+      <Card price={item.price} name={item.dishname} category={item.category} id={item._id} img={item.img}/>     
+      )
+    }
+  </div>;
+};
+
+export default AddButton;
